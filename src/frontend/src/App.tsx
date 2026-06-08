4191b762f072278fab6210b91bd09bb4cfa863d2
ZiYue1129 <james911129@gmail.com>
2026-06-08 12:39:36 +0800

    feat: update App routing with AuthProvider + Layout + new pages

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,18 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import BattlePage from "./pages/BattlePage";
-import LeaderboardPage from "./pages/LeaderboardPage";
-import ReplayPage from "./pages/ReplayPage";
+// src/frontend/src/App.tsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import Layout from './components/Layout'
+import HomePage from './pages/HomePage'
+import BattlePage from './pages/BattlePage'
+import LeaderboardPage from './pages/LeaderboardPage'
+import ReplayPage from './pages/ReplayPage'
+import ProfilePage from './pages/ProfilePage'
+import HistoryPage from './pages/HistoryPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/battle/:matchId" element={<BattlePage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
-        <Route path="/replay/:matchId" element={<ReplayPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/battle/:matchId" element={<BattlePage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+            <Route path="/replay/:matchId" element={<ReplayPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/history" element={<HistoryPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
 }
